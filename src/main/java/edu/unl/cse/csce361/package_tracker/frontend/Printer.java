6eb34a2a6a649ff426cc7cf95da1c0a594be08d6
Pengxiang Zhang <pengxiangzhang@huskers.unl.edu>
2019-12-02 15:57:40 -0600

    Add method to print warehouse for user to select.

--- src/main/java/edu/unl/cse/csce361/package_tracker/frontend/Printer.java
+++ src/main/java/edu/unl/cse/csce361/package_tracker/frontend/Printer.java
@@ -1,8 +1,7 @@
 package edu.unl.cse.csce361.package_tracker.frontend;
 
-
 public class Printer {
-	
+
 	public static void printMainMenu() {
 		System.out.println("Welcome, you're options are as follows:");
 		System.out.println("1. Log in as admin.");
@@ -20,7 +19,6 @@ public class Printer {
 		System.out.println("you're options are as follows:");
 	}
 
-
 	public static void printVIPMenu() {
 		System.out.println("Enter your name:");
 	}
@@ -28,8 +26,33 @@ public class Printer {
 	public static void printExit() {
 		System.out.println("Thank you for using our program. Terminating");
 	}
-	
+
 	public static void printInvalid() {
 		System.out.println("Error: invaild input");
+	}
+
+	public static void printWarehouse() {
+		System.out.println(String.format("%-5s %-30s %-50s", "ID", "Name", "Address"));
+		System.out.println(
+				String.format("%-5s %-30s %-50s", "1", "Seward", "Weller Hall, 800 N Columbia Ave, Seward, NE 68434"));
+		System.out.println(String.format("%-5s %-30s %-50s", "2", "Milford", "923 238th Rd, Milford, NE 68405"));
+		System.out.println(
+				String.format("%-5s %-30s %-50s", "3", "Lincoln 112st", "790-926 NW 112th St, Lincoln, NE 68528"));
+		System.out.println(String.format("%-5s %-30s %-50s", "4", "Lincoln Hub", "2701 O St, Lincoln, NE 68510"));
+		System.out.println(String.format("%-5s %-30s %-50s", "5", "O and 84",
+				"Weller Hall, 800 N Columbia Ave, Seward, NE 68434"));
+		System.out.println(
+				String.format("%-5s %-30s %-50s", "6", "O and Nebraska", "8525 Andermatt Drive, Lincoln, NE 68526"));
+		System.out.println(
+				String.format("%-5s %-30s %-50s", "7", "I80 and 154 st", "14541 Castlewood St, Waverly, NE 68462"));
+		System.out.println(
+				String.format("%-5s %-30s %-50s", "8", "I80 and 250 st", "14599 250th St, Greenwood, NE 68366"));
+		System.out.println(String.format("%-5s %-30s %-50s", "9", "I80 and Nebraska Crossing",
+				"21209 Nebraska Crossing Dr, Gretna, NE 68028"));
+		System.out.println(
+				String.format("%-5s %-30s %-50s", "10", "I80 and 118th St", "6271 S 118th St, Omaha, NE 68137"));
+		System.out.println(String.format("%-5s %-30s %-50s", "11", "Omaha Hub", "3110 Farnam St, Omaha, NE 68131"));
+		System.out
+				.println(String.format("%-5s %-30s %-50s", "12", "Missouri River", "4501 Abbott Dr, Omaha, NE 68110"));
 	}
 }
