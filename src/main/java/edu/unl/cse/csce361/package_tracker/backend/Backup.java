f0ecc6c63a04dd9b55348decc6f4d2a4df23fa9e
Xinyi <zhuxinyishcn@outlook.com>
2019-12-12 20:04:43 -0600

    add more geo info in backup script

--- src/main/java/edu/unl/cse/csce361/package_tracker/backend/Backup.java
+++ src/main/java/edu/unl/cse/csce361/package_tracker/backend/Backup.java
@@ -8,39 +8,39 @@ public class Backup {
 
     public static void initialDataBase () throws NoSuchAlgorithmException {
         SecureRandom random1 = SecureRandom.getInstance("SHA1PRNG");
-        Address address1 = new Address("1400 R St, Lincoln, NE 68588", "Lincoln", "68508");
-        Sender sender1 = new Sender(address1, "UNL", "sxc258");
-        Address address2 = new Address("122 East Jorgenson Street", "Chicago", "60613");
-        Receiver receiver1 = new Receiver(address2, "dddsx258", Math.abs(random1.nextInt() % 8));
-        BACKEND_FACADE.addPackageRecord(sender1, receiver1, Math.abs(random1.nextInt() % 8), Math.abs(random1.nextInt() % 20000));
-        Address address3 = new Address("1705 Arbor Dr, Lincoln", "Lincoln", "68503");
+//        Address address1 = new Address("1400 R St, Lincoln, NE 68588", "Lincoln", "68508");
+//        Sender sender1 = new Sender(address1, "UNL", "sxc258");
+//        Address address2 = new Address("122 East Jorgenson Street", "Chicago", "60613");
+//        Receiver receiver1 = new Receiver(address2, "dddsx258", Math.abs(random1.nextInt() % 8));
+//        BACKEND_FACADE.addPackageRecord(sender1, receiver1, Math.abs(random1.nextInt() % 8), Math.abs(random1.nextInt() % 20000));
+        Address address3 = new Address("1705 Arbor Dr, Lincoln", "Lincoln", "68503",40.832401,-96.678527);
         Sender sender2 = new Sender(address3, "University of Nebraska - Lincoln East Union", "unl-east");
-        Address address4 = new Address("187 N 2nd Street", "Lincoln", "68116");
+        Address address4 = new Address("187 N 2nd Street", "Lincoln", "68116",40.847067,-96.680766);
         Receiver receiver2 = new Receiver(address4, "Bryan", Math.abs(random1.nextInt() % 8));
         BACKEND_FACADE.addPackageRecord(sender2, receiver2, Math.abs(random1.nextInt() % 8), Math.abs(random1.nextInt() % 20000));
-        Address address5 = new Address("6001 Dodge Street", "Omaha", "68182");
+        Address address5 = new Address("6001 Dodge Street", "Omaha", "68182",40.834052,-96.645627);
         Sender sender3 = new Sender(address5, "University of Nebraska - Omaha", "uno");
-        Address address6 = new Address("8896 East 1st Ave.", "Dallas", "75001");
+        Address address6 = new Address("8896 East 1st Ave.", "Dallas", "75001",41.233041,-96.148515);
         Receiver receiver3 = new Receiver(address6, "Ben", Math.abs(random1.nextInt() % 8));
         BACKEND_FACADE.addPackageRecord(sender3, receiver3, Math.abs(random1.nextInt() % 8), Math.abs(random1.nextInt() % 20000));
-        Address address7 = new Address("42nd and Emile", "Omaha", "68198");
+        Address address7 = new Address("42nd and Emile", "Omaha", "68198",41.243361,-96.152644);
         Sender sender4 = new Sender(address7, "University of Nebraska Medical Center", "medical center");
-        Address address8 = new Address("4235 Pepper Rd", "Barton", "05822");
+        Address address8 = new Address("4235 Pepper Rd", "Lincoln", "68188", 40.845456,-96.670635 );
         Receiver receiver4 = new Receiver(address8, "James", Math.abs(random1.nextInt() % 8));
         BACKEND_FACADE.addPackageRecord(sender4, receiver4, Math.abs(random1.nextInt() % 8), Math.abs(random1.nextInt() % 20000));
-        Address address9 = new Address("12916 Millard Airport Plaza", "Omaha", "68137");
+        Address address9 = new Address("12916 Millard Airport Plaza", "Omaha", "68137",41.25166,-96.035793);
         Sender sender5 = new Sender(address9, "Millard Airport", "millard airport");
-        Address address10 = new Address("184 Marvel Way", "New York", "10453");
+        Address address10 = new Address("184 Marvel Way", "Lincoln", "68137",41.267158,-95.935291);
         Receiver receiver5 = new Receiver(address10, "Howard", Math.abs(random1.nextInt() % 8));
         BACKEND_FACADE.addPackageRecord(sender5, receiver5, Math.abs(random1.nextInt() % 8), Math.abs(random1.nextInt() % 20000));
-        Address address11 = new Address("4501 Abbott Dr", "Omaha", "68110");
+        Address address11 = new Address("4501 Abbott Dr", "Omaha", "68110",41.246377,-95.996423);
         Sender sender6 = new Sender(address11, "Millard Airport East", "millard airport-east");
-        Address address12 = new Address("4 Red Jazz Way", "Toronto", "M5V1J1");
+        Address address12 = new Address("4 Red Jazz Way", "Toronto", "M5V1J1",41.225814,-95.978561);
         Receiver receiver6 = new Receiver(address12, "Howard", Math.abs(random1.nextInt() % 8));
         BACKEND_FACADE.addPackageRecord(sender6, receiver6, Math.abs(random1.nextInt() % 8), Math.abs(random1.nextInt() % 20000));
-        Address address13 = new Address("16600 Quarry Oaks Dr Ashland", "Ashland", "68003");
+        Address address13 = new Address("16600 Quarry Oaks Dr Ashland", "Ashland", "68003",41.070591,-96.584934);
         Sender sender7 = new Sender(address13, "Quarry Oaks Golf Club", "golf for ever");
-        Address address14 = new Address("366 Fengyuan Way", "Shanghai", "200092");
+        Address address14 = new Address("366 Fengyuan Way", "Shanghai", "200092",40.751211,-96.187052);
         Receiver receiver7 = new Receiver(address14, "Bicker", Math.abs(random1.nextInt() % 8));
         BACKEND_FACADE.addPackageRecord(sender7, receiver7, Math.abs(random1.nextInt() % 8), Math.abs(random1.nextInt() % 20000));
         BACKEND_FACADE.closeConnection();
@@ -89,6 +89,6 @@ public class Backup {
 
     public static void main (String[] args) throws NoSuchAlgorithmException {
         initialDataBase();
-        initialWarehous();
+//        initialWarehous();
     }
 }
